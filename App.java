1f7e0885ab8c7c25910d70d3a4134d8b3a66f0a2
nm2562 <nm2562@srmist.edu.in>
2026-03-25 14:04:01 +0530

    feature/UC6: Dynamic banner for any word

--- App.java
+++ App.java
@@ -3,14 +3,23 @@ class App
 {
     public static void main( String[] args )
     {
-        StringBuilder sb= new StringBuilder("Hello");
+        StringBuilder sb= new StringBuilder("Hello, ");
+        String name = " ";
         if (args.length == 0)
-            sb.append(", World");
+            sb.append("World");
         else
             for (int i=0;i<args.length;i++)
             {
-                sb.append(", "+args[i]);
+                sb.append(args[i] + ", ");
             }
-        System.out.println( sb + "!");
+        if (sb.length()>12)
+        {
+            name=sb.substring(0,sb.length()-2);
+            System.out.println( name + "!");
+        }
+        else
+            System.out.println( sb + "!");
+
+
     }
 }
