cf475cb319ad11a15dd5350851680d6853af0837
nm2562 <nm2562@srmist.edu.in>
2026-03-25 13:53:15 +0530

    feature/UC4: Dynamic banner for any word

--- App.java
+++ App.java
@@ -1,10 +1,6 @@
 public class App {
     public static void main(String[] args) {
-        String name = "World";
-        if (args.length > 0) {
-            name = args[0];
-        }
-
+        String name = args[0];
         System.out.println("Hello, " + name + "!");
     }
 }
