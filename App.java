1a0b752dcfcea2cafb25f55e9be9651328657f26
nm2562 <nm2562@srmist.edu.in>
2026-03-25 14:01:06 +0530

    feature/C5: Dynamic banner for any word

--- App.java
+++ App.java
@@ -1,6 +1,16 @@
-public class App {
-    public static void main(String[] args) {
-        String name = args[0];
-        System.out.println("Hello, " + name + "!");
+package org.example;
+class App
+{
+    public static void main( String[] args )
+    {
+        StringBuilder sb= new StringBuilder("Hello");
+        if (args.length == 0)
+            sb.append(", World");
+        else
+            for (int i=0;i<args.length;i++)
+            {
+                sb.append(", "+args[i]);
+            }
+        System.out.println( sb + "!");
     }
 }
